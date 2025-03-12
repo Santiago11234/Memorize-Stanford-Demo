a47c12e1574e9a014ba58f2f3da099336379baff
Santiago <santiagogeb@gmail.com>
2025-03-12 12:22:25 -0500

    Stanford Lecture 6

---
+++ Memorize-Stanford-Demo/AspectVGrid.swift
@@ -0,0 +1,18 @@
+//
+//  aspectVGrid.swift
+//  Memorize-Stanford-Demo
+//
+//  Created by Santiago Espinoza on 3/12/25.
+//
+
+import SwiftUI
+
+struct aspectVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    aspectVGrid()
+}
